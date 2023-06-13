3f7e99a92a3aab0c272911279971fced65440c09
Jerry Sin <51seo.ca@gmail.com>
2023-06-13 02:09:35 -0400

    Google Dev Tool Script Recorder

--- index.js
+++ index.js
@@ -2,16 +2,34 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   // Launch a new browser instance
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: 'new'});
 
   // Create a new page
   const page = await browser.newPage();
 
+  // Set the page timeout to 15 seconds
+  const timeout = 15000;
+  page.setDefaultTimeout(timeout);
+
+  // Set the device dimensions
+  await page.setViewport(
+    {   width: 1024, 
+         height: 768
+});
+
   // Navigate to the URL
-  await page.goto('https://www.midjourny.com');
+  await page.goto('https://www.microsoft.com');
+
+  await page.waitForTimeout(1000);
+
+  _title = await page.title();
 
   // Perform further automation tasks on the page if needed
+  await page.screenshot({path: './downloads/full.png', fullPage: true});
 
   // Close the browser
   await browser.close();
+  console.log(_title);
+
 })();
+
